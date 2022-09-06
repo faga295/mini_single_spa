ec193124168b16dc3238185775152449eafc0ac9
lzc <liuzhaochen.faga@bytedance.com>
2022-09-06 22:36:20 +0800

    docs: update README

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # mini single-spa
 此项目用于学习single-spa，因为感觉看源码有点累，就想着写一个简版的。
+
 此项目使用pnpm来做monorepo,在examples里有使用vue搭建的微前端应用。
+
 启动项目
 ```
 pnpm run start
--- src/lifecycles/unmount.ts
+++ src/lifecycles/unmount.ts
@@ -1,6 +1,5 @@
 import { Application } from '../types/application';
 import { NOT_MOUNTED } from '../applications/app.helper';
-import * as singleSpa from '../index'
 export function toUnmountPromise(app:Application){
     return Promise.resolve().then(()=>{
         app.status = NOT_MOUNTED;
